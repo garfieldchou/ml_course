304c47efd97c698a339c826a01bd0db5492bdc1e
shaudai <shaudai0402@hotmail.com>
2017-05-12 07:09:57 +0800

    Fix typo

--- costFunctionJ.m
+++ costFunctionJ.m
@@ -1,7 +1,7 @@
 function J = costFunctionJ(X, y, theta)
 
 m = size(X,1);
-pridictions = X * theta;
-sqrErrors = (pridictions - y) .^ 2;
+predictions = X * theta;
+sqrErrors = (predictions - y) .^ 2;
 
 J = 1/(2*m) * sum(sqrErrors);
